8cd75d8d5dd796d9db7eb98c962f1e99ce198ece
catarfish <catarina.pien@gmail.com>
2020-03-27 16:33:34 -0700

    Updated version of app with anomalize: Much better!!

--- WaterTemp/app.R
+++ WaterTemp/app.R
@@ -2,7 +2,6 @@
 # Catarina Pien 
 # 12/11/2019
 
-
 # Set up the environment----------------------------------------------------------------------------
 
 # Clear the environment
@@ -10,11 +9,12 @@ rm(list=ls(all=TRUE))
 
 # Load packages
 library(shiny)
-library(readr)
 library(tidyverse)
 library(lubridate)
 library(scales)
 library(TTR)
+library(anomalize) # rate of change
+library(tibbletime) # rate of change - tables
 
 # Read in file --------------------------------------------------------------
 setwd("C:/Users/cpien/OneDrive - California Department of Water Resources/Work/ClimateChange/R_code/climatechange/")
@@ -59,12 +59,12 @@ ui <- fluidPage(
       numericInput("repeatvals",
                    "Repeating values allowed:",
                    min = 0, max = 24, value = 18),
-      numericInput("nsdev",
-                   "Rate of change: Number of standard deviations allowed from time average:",
-                   min = 0, max = 5, value = 3),
-      numericInput("pasthours", 
-                   "Rate of Change: Number of hours averaged for rate of change:",
-                   min = 0, max = 720, value = 25),
+      selectInput("seasonal",
+                  "Seasonal Decomposition Method (Loess = STL, Median = Twitter):",
+                  list("STL", "Twitter")),
+      selectInput("remainder",
+                  "Remainder Analysis Type (IQR: 3x above, 3x below IQR, GESD: progressively removes critical values, iterative:",
+                  list("IQR", "GESD")),
       sliderInput("temprange",
                   "Temperature Cutoffs:",
                   min = -10, max = 100,value = c(0,40)),
@@ -115,6 +115,7 @@ server <- function(input, output) {
     
     ### QC1: Remove rows with x number of missing values ########################################
     temp_q1_a <- temp_sta %>%
+      filter(!is.na(datetime)) %>%
       group_by(station, date) %>%
       arrange(station, date, hour) %>%
       summarise(total = length(date)) %>%
@@ -142,97 +143,28 @@ server <- function(input, output) {
     
     ### QC3: Filter for rate of change #############################################
     
-    # Calculate difference between temp and the previous temp record
-    # Calculate sd threshold based on user input (# sdev, # hours)
-    # If temp diff > sdev threshold or temp diff > 5, flag Y; otherwise flag N
-    # Replace NAs with N
-    temp_q3_a <- temp_q2 %>%
-      group_by(station) %>%
-      arrange(station, datetime) %>%
-      mutate(QC3 = abs(Temp- lag(Temp, n = 1, default = 0)))%>%
-      mutate(sdev_th = isolate(input$nsdev) * runSD(x = Temp, n = isolate(input$pasthours))) %>%
-      mutate(Flag_sdev = ifelse((QC3 > sdev_th | QC3 > 5), "Y", "N"))  %>%
-      mutate(Flag_sdev = replace(Flag_sdev, is.na(Flag_sdev), "N")) %>%
-      ungroup()
+    temp_q3_a <- as_tbl_time(temp_q2, index = datetime)
     
-    # This would address situations in which there is a single point that is influencing
-    # sd calculation for later values - redo calculations based on unflagged data. Will need to edit
-    # variable names 
-    
-    # temp_q3_a2 <- temp_q3_a %>%
-    #   filter(Flag_QC3a == "N") %>%
-    #   group_by(station) %>%
-    #   arrange(station, datetime) %>%
-    #   mutate(QC3 = abs(Temp- lag(Temp, n = 1, default = 0)))%>%
-    #   mutate(sdev_th = isolate(input$nsdev) * runSD(x = Temp, n = isolate(input$pasthours))) %>%
-    #   mutate(Flag_sdev = ifelse((QC3 > sdev_th | QC3 > 5), "Y", "N"))  %>%
-    #   mutate(Flag_sdev = replace(Flag_sdev, is.na(Flag_sdev), "N")) %>%
-    #   ungroup()
-    
-    # Remove unnecessary variables
     temp_q3_c <- temp_q3_a %>%
-      select(-c(QC3))
+      # Data Manipulation / Anomaly Detection
+      time_decompose(Temp, method = isolate(input$seasonal)) %>%
+      anomalize(remainder, method = isolate(input$remainder)) %>%
+      time_recompose() %>% select(c(datetime, anomaly)) %>%
+      as_tibble()
     
-    # Cumsum every time there is a flagged value to separate data into different groups
-    # each time there is a spike
-    temp_q3_d = temp_q3_c %>%
-      mutate(Flag_spike = cumsum(Flag_sdev == "Y"))
-
-    # Compare groups of values with each other 
-    # Compare first temp of a group with last temp of previous group (t-1), (t-2)
-    # Compare last temp of a group with first temp of subsequent group (t+1)
-    # If all of these temp diffs are > sdev_th AND temp diff (t-1) >2 ||| OR temp diff(t-1) >5, flag data.
-    # The clause of temp diff (t-1) > 2 helps eliminate much unnecessarily flagged data
-    temp_q3_e <- temp_q3_d %>%
-      group_by(station, Flag_spike) %>%
-      summarize(tempi = first(Temp),
-                tempf = last(Temp),
-                sdev_thlast = round(last(sdev_th),3),
-                n = n()) %>%
-      mutate(tempfprev = lag(tempf, 1),
-             tempfprev2 = lag(tempf, 2),
-             tempinext = lead(tempi, 1),
-             temp_diff1 = abs(tempi - tempfprev),
-             temp_diff2 = abs(tempi - tempfprev2),
-             temp_diffnext = abs(tempinext-tempf),
-             sdev_thprev = lag(sdev_thlast, 1),
-             sdev_thprev2 = lag(sdev_thlast,2)) %>%
-      mutate(Flag_QC3 = ifelse((temp_diff1 > sdev_thprev & temp_diff2 > sdev_thprev2 & 
-                                  temp_diffnext > sdev_thlast & temp_diff1>2 & temp_diffnext >2) | temp_diff1 > 5, "Y", "N" ))
-
-    # Merge summarized calculations from e back in with rest of the data
-    temp_q3 <- left_join(temp_q3_d, temp_q3_e, by = c("station", "Flag_spike"))%>%
-      select(-c(tempi:sdev_thprev2)) 
     
-    ## Only needed if doing the temp_q3_a_2 flag
-    #temp_q3_f_merge <- temp_q3_f %>%select(station, datetime, Flag_spike, Flag_QC3)
     
-    #temp_q3 <- left_join(temp_q3_a, temp_q3_f_merge) #%>%
-     # mutate(Flag_QC3 = replace(Flag_QC3, Flag_QC3a == "Y", "Y"))
+    temp_q3_d <- inner_join(temp_q2, temp_q3_c, by = c( "datetime"))
+    
+    temp_q3 <- temp_q3_d %>%
+      mutate(anomaly = factor(anomaly)) %>%
+      mutate(anomaly = recode(anomaly, No = "N", Yes = "Y"))  %>%
+      rename(Flag_QC3 = anomaly) 
+    
     
     # Flagged values
     temp_q3_b <- temp_q3 %>%
       filter(Flag_QC3 == "Y")
-    
-    
-    # # QC4: Filter for rate of change #############################################
-    # 
-    # temp_q4_a <- temp_q3 %>%
-    #   group_by(station) %>%
-    #   arrange(station, datetime) %>%
-    #   mutate(QC4 = abs(Temp- lag(Temp, n = 1))) %>%
-    #   mutate(sdev_th = isolate(input$nsdev) * runSD(x = Temp, n = isolate(input$pasthours))) %>%
-    #   mutate(Flag_QC3 = ifelse(QC3 > sdev_th | QC3 >5, "Y", "N")) %>%
-    #   ungroup()
-    # 
-    # # Remove a few columns
-    # temp_q3 <- temp_q3_a %>%
-    #   select(-c(sdev_th, QC3))
-    # 
-    # # Create a dataframe of all the "delete" rows
-    # temp_q3_b <- temp_q3 %>%
-    #   filter(Flag_QC3 == "Y")
-    # 
     
     
     # # QC4: Range for acceptable temperatures ###########################################################
@@ -282,7 +214,7 @@ server <- function(input, output) {
       ranges$y <- NULL
     }
   })
-    
+  
   ############################################################################
   ## Plot2: PostQC Final ----------------------------------------------------------------
   ##############################################################################
@@ -327,99 +259,28 @@ server <- function(input, output) {
     
     ### QC3: Filter for rate of change #############################################
     
-    # Calculate difference between temp and the previous temp record
-    # Calculate sd threshold based on user input (# sdev, # hours)
-    # If temp diff > sdev threshold or temp diff > 5, flag Y; otherwise flag N
-    # Replace NAs with N
-    temp_q3_a <- temp_q2 %>%
-      group_by(station) %>%
-      arrange(station, datetime) %>%
-      mutate(QC3 = abs(Temp- lag(Temp, n = 1, default = 0)))%>%
-      mutate(sdev_th = isolate(input$nsdev) * runSD(x = Temp, n = isolate(input$pasthours))) %>%
-      mutate(Flag_sdev = ifelse((QC3 > sdev_th | QC3 > 5), "Y", "N"))  %>%
-      mutate(Flag_sdev = replace(Flag_sdev, is.na(Flag_sdev), "N")) %>%
-      ungroup()
+    temp_q3_a <- as_tbl_time(temp_q2, index = datetime)
     
-    # This would address situations in which there is a single point that is influencing
-    # sd calculation for later values - redo calculations based on unflagged data. Will need to edit
-    # variable names 
-    
-    # temp_q3_a2 <- temp_q3_a %>%
-    #   filter(Flag_QC3a == "N") %>%
-    #   group_by(station) %>%
-    #   arrange(station, datetime) %>%
-    #   mutate(QC3 = abs(Temp- lag(Temp, n = 1, default = 0)))%>%
-    #   mutate(sdev_th = isolate(input$nsdev) * runSD(x = Temp, n = isolate(input$pasthours))) %>%
-    #   mutate(Flag_sdev = ifelse((QC3 > sdev_th | QC3 > 5), "Y", "N"))  %>%
-    #   mutate(Flag_sdev = replace(Flag_sdev, is.na(Flag_sdev), "N")) %>%
-    #   ungroup()
-    
-    # Remove unnecessary variables
     temp_q3_c <- temp_q3_a %>%
-      select(-c(QC3))
+      # Data Manipulation / Anomaly Detection
+      time_decompose(Temp, method = isolate(input$seasonal)) %>%
+      anomalize(remainder, method = isolate(input$remainder)) %>%
+      time_recompose() %>% select(c(datetime, anomaly)) %>%
+      as_tibble()
     
-    # Cumsum every time there is a flagged value to separate data into different groups
-    # each time there is a spike
-    temp_q3_d = temp_q3_c %>%
-      mutate(Flag_spike = cumsum(Flag_sdev == "Y"))
     
-    # Compare groups of values with each other 
-    # Compare first temp of a group with last temp of previous group (t-1), (t-2)
-    # Compare last temp of a group with first temp of subsequent group (t+1)
-    # If all of these temp diffs are > sdev_th AND temp diff (t-1) >2 ||| OR temp diff(t-1) >5, flag data.
-    # The clause of temp diff (t-1) > 2 helps eliminate much unnecessarily flagged data
-    temp_q3_e <- temp_q3_d %>%
-      group_by(station, Flag_spike) %>%
-      summarize(tempi = first(Temp),
-                tempf = last(Temp),
-                sdev_thlast = round(last(sdev_th),3),
-                n = n()) %>%
-      mutate(tempfprev = lag(tempf, 1),
-             tempfprev2 = lag(tempf, 2),
-             tempinext = lead(tempi, 1),
-             temp_diff1 = abs(tempi - tempfprev),
-             temp_diff2 = abs(tempi - tempfprev2),
-             temp_diffnext = abs(tempinext-tempf),
-             sdev_thprev = lag(sdev_thlast, 1),
-             sdev_thprev2 = lag(sdev_thlast,2)) %>%
-      mutate(Flag_QC3 = ifelse((temp_diff1 > sdev_thprev & temp_diff2 > sdev_thprev2 & 
-                                  temp_diffnext > sdev_thlast & temp_diff1>2 & temp_diffnext >2) | temp_diff1 > 5, "Y", "N" ))
     
-    # Merge summarized calculations from e back in with rest of the data
-    temp_q3 <- left_join(temp_q3_d, temp_q3_e, by = c("station", "Flag_spike"))%>%
-      select(-c(tempi:sdev_thprev2)) 
+    temp_q3_d <- inner_join(temp_q2, temp_q3_c, by = c( "datetime"))
     
-    ## Only needed if doing the temp_q3_a_2 flag
-    #temp_q3_f_merge <- temp_q3_f %>%select(station, datetime, Flag_spike, Flag_QC3)
+    temp_q3 <- temp_q3_d %>%
+      mutate(anomaly = factor(anomaly)) %>%
+      mutate(anomaly = recode(anomaly, No = "N", Yes = "Y"))  %>%
+      rename(Flag_QC3 = anomaly) 
     
-    #temp_q3 <- left_join(temp_q3_a, temp_q3_f_merge) #%>%
-    # mutate(Flag_QC3 = replace(Flag_QC3, Flag_QC3a == "Y", "Y"))
-  
+    
     # Flagged values
     temp_q3_b <- temp_q3 %>%
-      filter(Flag_QC3 == "Y") 
-    
-    
-    
-    
-    # # QC4: Filter for rate of change #############################################
-    # 
-    # temp_q4_a <- temp_q3 %>%
-    #   group_by(station) %>%
-    #   arrange(station, datetime) %>%
-    #   mutate(QC4 = abs(Temp- lag(Temp, n = 1))) %>%
-    #   mutate(sdev_th = isolate(input$nsdev) * runSD(x = Temp, n = isolate(input$pasthours))) %>%
-    #   mutate(Flag_QC3 = ifelse(QC3 > sdev_th | QC3 >5, "Y", "N")) %>%
-    #   ungroup()
-    # 
-    # # Remove a few columns
-    # temp_q3 <- temp_q3_a %>%
-    #   select(-c(sdev_th, QC3))
-    # 
-    # # Create a dataframe of all the "delete" rows
-    # temp_q3_b <- temp_q3 %>%
-    #   filter(Flag_QC3 == "Y")
-    # 
+      filter(Flag_QC3 == "Y")
     
     # # QC4: Range for acceptable temperatures ###########################################################
     temp_q4 <- temp_q3%>%
@@ -434,8 +295,6 @@ server <- function(input, output) {
     temp_q4_b <- temp_q4 %>%
       filter(Flag_QC4 == "Y")
     
-    
-  
     ####################### EDITED ##############################################
     temp_q3_plot <- left_join(temp_q3_b, temp_q4) %>%
       filter(Flag_QC4 == "N")
@@ -541,17 +400,25 @@ server <- function(input, output) {
     
     # QC3: Filter for rate of change #############################################
     
-    temp_q3_a <- temp_q2 %>%
-      group_by(station) %>%
-      arrange(station, datetime) %>%
-      mutate(QC3 = abs(Temp- lag(Temp, n = 1))) %>%
-      mutate(sdev_th = isolate(input$nsdev) * runSD(x = Temp, n = isolate(input$pasthours))) %>%
-      mutate(Flag_QC3 = ifelse(QC3 > sdev_th | QC3 >5, "Y", "N")) %>%
-      ungroup()
+    temp_q3_a <- as_tbl_time(temp_q2, index = datetime)
     
-    # Remove a few columns
-    temp_q3 <- temp_q3_a %>%
-      select(-c(sdev_th, QC3))
+    temp_q3_c <- temp_q3_a %>%
+      # Data Manipulation / Anomaly Detection
+      time_decompose(Temp, method = isolate(input$seasonal)) %>%
+      anomalize(remainder, method = isolate(input$remainder)) %>%
+      time_recompose() %>% select(c(datetime, anomaly)) %>%
+      as_tibble()
+    
+    temp_q3_d <- inner_join(temp_q2, temp_q3_c, by = c( "datetime"))
+    
+    temp_q3 <- temp_q3_d %>%
+      mutate(anomaly = factor(anomaly)) %>%
+      mutate(anomaly = recode(anomaly, No = "N", Yes = "Y"))  %>%
+      rename(Flag_QC3 = anomaly) 
+    
+    # Flagged values
+    temp_q3_b <- temp_q3 %>%
+      filter(Flag_QC3 == "Y")
     
     # # QC4: Range for acceptable temperatures ###########################################################
     temp_q4 <- temp_q3%>%
