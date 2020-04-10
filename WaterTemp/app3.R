a14ab647af6d7d1a41b764869c83dc8fa90fbd7e
catarfish <catarina.pien@gmail.com>
2020-04-10 16:25:34 -0700

    Just moved a few things around, added rate of change back in to app

--- WaterTemp/app3.R
+++ WaterTemp/app3.R
@@ -51,29 +51,41 @@ ui <- fluidPage(
   sidebarLayout(
     position = "left",
     sidebarPanel(
-      h4("Filters"),
+      h3("Filters"),
+      h4("Station and Date"),
       uiOutput("station_selector"), # See station_selector in server section
       dateRangeInput("daterange", "Date Range:",
                      start = "2010-01-01", end = "2018-01-01", min = "1980-01-01", startview = "decade"),
-            sliderInput("temprange",
+      h4("1. Temperature cutoffs"),
+      sliderInput("temprange",
                   "Temperature Cutoffs:",
                   min = -10, max = 100,value = c(1,40)),
+      h4("2. Missing Values"),
       numericInput("missvals",
                    "Missing Values allowed per day:",
-                   min = 0, max = 24, value = 5),
+                   min = 0, max = 24, value = 4),
+      h4("3. Repeating Values"),
       numericInput("repeatvals",
                    "Repeating values allowed:",
                    min = 0, max = 24, value = 18),
+      h4("4. Anomaly Detection"),
       selectInput("trend",
                   "Trend duration",
-                  list("2 weeks", "1 month", "2 months", "3 months", "4 months", "5 months","6 months", "1 year", 
-                       startview = "4 months")),
+                  list("2 weeks", "1 month", "2 months", "3 months", "4 months", "5 months","6 months", "1 year"), 
+                       selected = "3 months"),
       selectInput("seasonal",
                   "Seasonal Decomposition Method (Loess = STL, Median = Twitter):",
                   list("STL", "Twitter")),
       selectInput("remainder",
                   "Remainder Analysis Type (IQR: 3x above, 3x below IQR, GESD: progressively removes critical values, iterative:",
                   list("IQR", "GESD")),
+      h4("5. Rate of Change"),
+      numericInput("nsdev",
+                   "Rate of change: Number of standard deviations allowed from time average:",
+                   min = 0, max = 5, value = 5),
+      numericInput("pasthours", 
+                   "Rate of Change: Number of hours averaged for rate of change:",
+                   min = 0, max = 720, value = 50),
 
       actionButton("submit", "Submit")
     ),
@@ -81,23 +93,28 @@ ui <- fluidPage(
     # main panel
     mainPanel(h3("Values Flagged"),
               tableOutput("vals_removed"),
-              h3("Plot: Pre-QC with flagged values"),
-              p(strong("To zoom in, highlight points, then double click inside")),
+              h3("Plot 1: Pre-QC with flagged values"),
+              p(strong("To zoom in, highlight points, then double click inside box. Zoom works on
+              both plot 1 and plot 2, but output will show up in plot 2.")),
               
-              p(strong(span("Blue", style = "color:darkturquoise")),
+              p(strong(span("Blue x", style = "color:darkturquoise")),
               "= Temperature limits"),
-              p(strong(span("Yellow", style= "color:gold")),
+              p(strong(span("Yellow triangle", style= "color:gold")),
               "= Missing values"),
-              p(strong(span("Red", style = "color:firebrick")),
+              p(strong(span("Red circle", style = "color:firebrick")),
               "= Repeating values"),
-              p(strong(span("Green", style = "color:seagreen")),
-              "= Rate of Change/ Anomalies"), 
+              p(strong(span("Green diamond", style = "color:seagreen")),
+              "= Anomalies"), 
+              p(strong(span("Magenta square", style = "color:mediumorchid")),
+              "= Rate of Change"),
               
               plotOutput("preQC", dblclick = "preQC_dblclick",
                          brush = brushOpts(id = "preQC_brush",
                                            resetOnNew = TRUE)),
-              h3("Plot: Filtered for temperature limits"),
-              plotOutput("postQC_F")
+              h3("Plot 2: Filtered for temperature limits"),
+              plotOutput("postQC_F", dblclick = "preQC_dblclick",
+                         brush = brushOpts(id = "preQC_brush",
+                                           resetOnNew = TRUE))
               
     )
   )
@@ -115,26 +132,26 @@ server <- function(input, output) {
                 selected = "ANC") #default choice (not required)
   })
   
- 
+  ### Define reactive values for the zoom function
   ranges <- reactiveValues(x = NULL, y = NULL)
   
   
   ### Filter station and dates from input
   temp_sta <-  eventReactive(input$submit, {
     temp_H %>%
+      filter(!is.na(datetime)) %>% # This is important for the anomalize part
       filter(station == input$station) %>%
       filter(date >= input$daterange[1] & date <= input$daterange[2]) 
   })
   
-  ### QC Range for acceptable temperatures ###
+  ### QC1) Range for acceptable temperatures ###
   temp_q1 <- eventReactive(input$submit, { 
     temp_sta() %>%
-      filter(!is.na(datetime)) %>%
       mutate(Flag_QC1 = ifelse((Temp<input$temprange[1] | Temp>input$temprange[2]), "Y", "N"))
     
   })
   
-  ### Missing values ###
+  ### QC2) Missing values ###
   temp_q2_a <- eventReactive(input$submit,{
     temp_q1() %>%
       group_by(station, date) %>%
@@ -147,13 +164,14 @@ server <- function(input, output) {
     dplyr::left_join(temp_q1(), temp_q2_a(), by = c("station", "date"))
   })
 
-  ### Repeating Values
+  ### QC3) Repeating Values
   temp_q3 <- eventReactive(input$submit, {
     repeating_vals(df = temp_q2(), x = input$repeatvals)%>%
+    select(-flag, -issame, -same) %>%
     rename(Flag_QC3 = Flag_repeats) })
   
   
-  ### Anomalies
+  ### QC4) Anomalies
   temp_q4_a <- eventReactive(input$submit,{
     as_tbl_time(temp_q3(), index = datetime)})
   
@@ -175,6 +193,21 @@ server <- function(input, output) {
     rename(Flag_QC4 = anomaly) })
   
    
+  ### Rate of Change
+  temp_q5 <- eventReactive(input$submit, {
+    temp_q4() %>%
+      group_by(station) %>%
+      arrange(station, datetime) %>%
+      mutate(QC5 = abs(Temp- lag(Temp, n = 1, default = 0)))%>%
+      mutate(sdev_th = input$nsdev * runSD(Temp, input$pasthours))%>%
+      mutate(Flag_QC5 = ifelse((QC5 > sdev_th | QC5 > 5), "Y", "N"))  %>%
+      mutate(Flag_QC5 = replace(Flag_QC5, is.na(Flag_QC5), "N")) %>%
+      select(-QC5)%>%
+      ungroup()
+  })
+  
+
+  
   ### Filter all delete rows
   temp_q1_b <-  reactive( {
     temp_q1() %>%
@@ -192,6 +225,38 @@ server <- function(input, output) {
     temp_q4() %>%
     filter(Flag_QC4 == "Y") })
   
+  temp_q5_b <- reactive({
+    temp_q5() %>%
+      filter(Flag_QC5 == "Y")
+  })
+  
+  
+  # Plot 2: Within temperature limits 
+  # Filter out temperature range so outliers don't distort the plot to the point that it is 
+  # hard to see the actual trends 
+  
+  # For plot 2
+  temp_q5_plot <- eventReactive(input$submit, {
+    temp_q5() %>%
+      filter(Flag_QC1 =="N")
+  })
+  
+  temp_q2_b_plot <- reactive({
+    temp_q5_plot() %>%
+    filter(Flag_QC2 == "Y") })
+  
+  temp_q3_b_plot <- reactive({
+    temp_q5_plot() %>%
+    filter(Flag_QC3 == "Y") })
+  
+  temp_q4_b_plot <- reactive({
+    temp_q5_plot() %>%
+    filter(Flag_QC4 == "Y")})
+  
+  temp_q5_b_plot <- reactive({
+    temp_q5_plot() %>%
+    filter(Flag_QC5 == "Y")})
+  
   
   ###############################################################
   ## Table -------------------------------------------------------
@@ -203,14 +268,16 @@ server <- function(input, output) {
     
     # Make table
     # Counts the number of rows of data being flagged
-    vals_removed <- temp_q4() %>%
+    vals_removed <- temp_q5() %>%
       group_by(station) %>%
       summarize(Init = n(),
                 QC1 = sum(Flag_QC1 =="Y"),
                 QC2 = sum(Flag_QC2 == "Y"),
                 QC3 = sum(Flag_QC3 == "Y", na.rm = TRUE),
                 QC4 = sum(Flag_QC4 == "Y"),
-                Remaining = sum(Flag_QC1=="N" & Flag_QC2 == "N" & Flag_QC3 =="N" & Flag_QC4 =="N", na.rm = TRUE),
+                QC5 = sum(Flag_QC5 == "Y"),
+                Remaining = sum(Flag_QC1=="N" & Flag_QC2 == "N" & Flag_QC3 =="N" & Flag_QC4 =="N"
+                                & Flag_QC5 == "N", na.rm = TRUE),
                 Prop_remaining = round(Remaining/Init*100,1)) %>%
       arrange(Prop_remaining)
     
@@ -232,11 +299,12 @@ server <- function(input, output) {
     
     # Plot data
     ggplot() +
-      geom_point(data = temp_q4(), aes(datetime, Temp), col = "lightsteelblue3") +
-      geom_point(data = temp_q1_b(), aes(datetime, Temp), color = "cyan3", size = 2) +
-      geom_point(data = temp_q2_b(), aes(datetime, Temp), color = "goldenrod2", size = 2) +
-      geom_point(data = temp_q3_b(), aes(datetime, Temp), color = "indianred3", size = 2) +
-      geom_point(data = temp_q4_b(), aes(datetime, Temp), color = "springgreen4", size = 2) +
+      geom_point(data = temp_q5(), aes(datetime, Temp), col = "lightsteelblue3") +
+      geom_point(data = temp_q1_b(), aes(datetime, Temp), color = "cyan3", size = 4, shape = 4) +
+      geom_point(data = temp_q2_b(), aes(datetime, Temp), color = "goldenrod2", size = 3, shape = 2) +
+      geom_point(data = temp_q3_b(), aes(datetime, Temp), color = "indianred3", size = 3, shape = 20) +
+      geom_point(data = temp_q4_b(), aes(datetime, Temp), color = "springgreen4", size = 3.5, shape = 18) +
+      geom_point(data = temp_q5_b(), aes(datetime, Temp), color = "mediumorchid", size = 3, shape = 0) +
       coord_cartesian(xlim = ranges$x, ylim = ranges$y, expand = TRUE) +
       labs(y = "Temp (deg C)") +
       scale_x_datetime() + 
@@ -269,20 +337,16 @@ server <- function(input, output) {
   
   output$postQC_F <- renderPlot({
    
-    # Plot within temperature limits 
-    temp_q4_plot <- temp_q4() %>%
-      filter(Flag_QC1 == "N")
-    
-    temp_q4_b_plot <- temp_q4_plot %>%
-      filter(Flag_QC4 == "Y")
+ 
     
     # Plot the data ########################################################################################
     # Each geom_point() plots a different flag
     ggplot() +
-      geom_point(data = temp_q4_plot, aes(datetime, Temp), col = "lightsteelblue3") +
-      geom_point(data = temp_q2_b(), aes(datetime, Temp), color = "goldenrod2", size = 2) +
-      geom_point(data = temp_q3_b(), aes(datetime, Temp), color = "indianred3", size = 2) +
-      geom_point(data = temp_q4_b_plot, aes(datetime, Temp), color = "springgreen4", size = 2) +
+      geom_point(data = temp_q5_plot(), aes(datetime, Temp), col = "lightsteelblue3") +
+      geom_point(data = temp_q2_b_plot(), aes(datetime, Temp), color = "goldenrod2", size = 3, shape = 2) +
+      geom_point(data = temp_q3_b_plot(), aes(datetime, Temp), color = "indianred3", size = 3, shape = 20) +
+      geom_point(data = temp_q4_b_plot(), aes(datetime, Temp), color = "springgreen4", size = 3.5, shape = 18) +
+      geom_point(data = temp_q5_b_plot(), aes(datetime, Temp), color = "mediumorchid", size = 3, shape = 0) +
       labs(y = "Temp (deg C)") +
       scale_x_datetime() +
       theme_bw() +
@@ -291,6 +355,21 @@ server <- function(input, output) {
             axis.text.x = element_text(angle = 90, hjust = 1))
   })
   
+  
+  
+  # When a double-click happens, check if there's a brush on the plot.
+  # If so, zoom to the brush bounds; if not, reset the zoom.
+  observeEvent(input$preQC_dblclick, {
+    brush <- input$preQC_brush
+    if (!is.null(brush)) {
+      ranges$x <- c(brush$xmin, brush$xmax)
+      ranges$y <- c(brush$ymin, brush$ymax)
+      
+    } else {
+      ranges$x <- NULL
+      ranges$y <- NULL
+    }
+  })
 }
 
 # Run the application 
